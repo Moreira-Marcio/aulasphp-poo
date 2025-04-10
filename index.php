2d0ebbe2a01b75b8d7f8bd061c154b9298fba568
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-04-10 09:54:47 -0300

    altera exemplo 02

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 02</title>
+</head>
+<body>
+
+<h1>PHP com POO - Exemplo 02</h1>
+<hr>
 <?php
 // carregando/importando a classe
 require_once "src/Cliente.php";
@@ -14,21 +29,6 @@ $clienteB -> nome = "Zezinho";
 $clienteB -> idade = 25;
 $clienteB -> email = "zezinho@gmail.com"
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 01</title>
-</head>
-<body>
-
-<h1>PHP com POO - Exemplo 01</h1>
-<hr>
-
 <h2>Acessando/lendo os dados do objeto</h2>
 
 <h3>Cliente A</h3>
