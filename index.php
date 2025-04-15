da1797cd85faa07346668cadbfaa1c429f4e70d0
MARCIO MOREIRA <107818343+Moreira-Marcio@users.noreply.github.com>
2025-04-15 11:24:40 -0300

    realiza ajustes

--- index.php
+++ index.php
@@ -14,28 +14,6 @@
 <h1>PHP com POO - Exemplo 04</h1>
 <hr>
 <?php
-// carregando/importando a classe
-require_once "src/Cliente.php";
-//criar  objeto/instância da classe
-$clienteA = new Cliente("fulano",30,"fulano@gmail.com");
-$clienteB = new Cliente("zezinho",25,"zezinho@gmail.com");
-?>
-<h2>Acessando/lendo os dados do objeto</h2>
-<ul>
-    <li><b>Nome:</b><?=$clienteA->getNome()?></li>
-    <li><b>Idade:</b><?=$clienteA->getIdade()?></li>
-    <li><b>E-mail:</b><?=$clienteA->getEmail()?></li>
-    <li><b>Faixa etaria:</b> <?=$clienteA->verificarIdade()?></li>
-
-</ul>
-<ul>
-     <li><b>Nome:</b><?=$clienteB->getNome()?></li>
-    <li><b>Idade:</b><?=$clienteB->getIdade()?></li>
-    <li><b>E-mail:</b><?=$clienteB->getEmail()?></li>
-    <li><b>Faixa etaria:</b> <?=$clienteB->verificarIdade()?></li>
-
-</ul>
-<h2>teste de acesso aos recursos da classe atraves do objeto</h2>
 
 
 
--- src/Cliente.php
+++ src/Cliente.php
@@ -1,67 +1,47 @@
 <?php
 class Cliente
 {
-    //atributos (caracteristicas da classe)
-    private string $nome;
-    private int $idade;
-    private string $email;
-   // metodos setters: responsavel por receber/atribuir dados para propriedades /atributos*/
+     //atributos (caracteristicas da classe)
+     private string $nome;
+     private int $idade;
+     private string $email;
+     // metodos setters: responsavel por receber/atribuir dados para propriedades /atributos*/
 
-   //metodo construtor (sempre e executado automaticamente ao criar objeto)
-   public function __construct(string $nome, int $idade,string $email)
-   {
-     $this->setNome($nome);
-     $this->setIdade($idade);
-     $this->setEmail($email);
-   }
+     //metodo construtor (sempre e executado automaticamente ao criar objeto)
+     public function __construct(string $nome, string $email)
+     {
+          $this->setNome($nome);        
+          $this->setEmail($email);
+     }
 
-   public function verificarIdade():string{
-     if ($this->idade < 18) return "menor de idade";
-     if ($this->idade < 60) return "adulto";
 
-     return "idoso";
-   }
+     public function setNome(string $nome): void
+     {
 
-   public function setNome(string $nome): void
-   {
-         
-        $this->nome =$nome;
-    
-   }
-   
-   public function setIdade(int $idade): void 
-   {
-        if($idade <0){
-            throw new InvalidArgumentException("idade nao pode ser negativa");
-        }
-        $this->idade = $idade;
-   }
+          $this->nome = $nome;
+     }
 
-   public function setEmail(string $email): void 
-   {    
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL) ){
-            throw new InvalidArgumentException("e-mail invalido!");
-        }
-        $this->email = $email;
-   }
 
-   //metodos getters: responsaveis por acessar/ler os dados*/
 
-   public function getNome(): string
-   {
-        return $this->nome;
-    
-   }
+     public function setEmail(string $email): void
+     {
+          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+               throw new InvalidArgumentException("e-mail invalido!");
+          }
+          $this->email = $email;
+     }
 
-   public function getIdade(): int
-   {
-        return $this->idade;
+     //metodos getters: responsaveis por acessar/ler os dados*/
 
-   }
+     public function getNome(): string
+     {
+          return $this->nome;
+     }
 
-   public function getEmail(): string
-   {
-        return $this->email;
-   }
 
+
+     public function getEmail(): string
+     {
+          return $this->email;
+     }
 }
